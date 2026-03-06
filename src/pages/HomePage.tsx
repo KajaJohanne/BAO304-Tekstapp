bbadbd14bbf07393d585754bf456175873a9464a
KajaJohanne <kajajohanne@gmail.com>
2026-03-06 11:24:03 +0100

    legg til mockdata for applikasjon, og state i HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,35 @@
-// Homepage == Applications? 
+import { useState } from "react";
+import type { Application } from "../types";
+
+
+// Mockdata for å lage komponenten til listeelementet
+const mockApplications: Application[] = [
+    {
+        id: "1", 
+        name: "Trafikk", 
+        createdAt: new Date(), 
+        sections: [
+            { id: "s1", name: "Reiseinformasjon" }, 
+            { id: "s2", name: "Langs veien" }, 
+            { id: "s3", name: "Trafikksikkerhet" }
+        ]
+    }, 
+    {
+        id: "2", 
+        name: "Førerkort", 
+        createdAt: new Date(), 
+        sections: [], 
+    },
+];
 
 const HomePage = () => {
-    return <div>Home Page == oversikt over alle applikasjoner</div>
+    const [applications, setApplications] = useState<Application[]>(mockApplications); 
+
+    return (
+        <div>
+            <h1>Applikasjoner</h1>
+        </div>
+    )
 };
 
 export default HomePage; 
