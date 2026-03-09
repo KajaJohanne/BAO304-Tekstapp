abe28f40f12fb5537586fa8464e41001a154cbeb
sarasteinsland <sarasteinsland01@gmail.com>
2026-03-09 14:20:08 +0100

    lagt til bokmål nynorsk engelsk

--- src/pages/TextKeyDetailPage.tsx
+++ src/pages/TextKeyDetailPage.tsx
@@ -1,7 +1,51 @@
 // Her kommer detaljer over valgt tekstnøkkel 
 
-const TextKeyDetailPage = () => {
-    return <div>Detaljeside for valgt tekstnøkkel</div>;
-}; 
+import { useParams } from "react-router-dom";
 
-export default TextKeyDetailPage; 
+const TextKeyDetailPage = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="page">
+
+      <h1>Rediger tekstnøkkel</h1>
+
+      <h2>{id}</h2>
+
+      <div className="environment">
+        <button>UTV</button>
+        <button>TEST</button>
+        <button>PROD</button>
+      </div>
+
+      <h3>Overskrift</h3>
+
+      <div className="form">
+
+        <div className="row">
+          <label>Bokmål</label>
+          <input type="text" />
+        </div>
+
+        <div className="row">
+          <label>Nynorsk</label>
+          <input type="text" />
+        </div>
+
+        <div className="row">
+          <label>Engelsk</label>
+          <input type="text" />
+        </div>
+
+      </div>
+
+      <div className="buttons">
+        <button>Lagre</button>
+        <button>Avbryt</button>
+      </div>
+
+    </div>
+  );
+};
+
+export default TextKeyDetailPage;
