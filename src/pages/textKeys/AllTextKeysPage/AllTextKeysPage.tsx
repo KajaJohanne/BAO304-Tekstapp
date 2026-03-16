67d8489c80d3632154429a10f8a64787b4f270af
Katrine <kana020@student.kristiania.no>
2026-03-16 15:52:23 +0100

    Lagt til grunnleggende layout og header komponent

--- src/pages/textKeys/AllTextKeysPage/AllTextKeysPage.tsx
+++ src/pages/textKeys/AllTextKeysPage/AllTextKeysPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAllTextKeys, type TextKeyListItem } from "../../api";
+import { getAllTextKeys, type TextKeyListItem } from "../../../../api";
 
 const AllTextKeysPage = () => {
   const navigate = useNavigate();
